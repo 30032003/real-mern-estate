7730263063863ff4ba5d500fde37aefb26465544
DSA Notes <dahiwaleharshal9@gmail.com>
2025-01-29 12:23:55 +0530

    craete 1 app using tailwind css and mern stack

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Profile from './pages/Profile';  
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
 function App() {
   return (
-    <div>
-      <h2 clas="edit" color='red'>love you javascript</h2>
-    </div>
-  )
-};
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+      )
+}
 
 export default App
